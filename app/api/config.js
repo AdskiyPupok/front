93e957c87e4504ca7c59ddd3a84c19017a058fbf
AdskiyPupok <150174912+AdskiyPupok@users.noreply.github.com>
2024-05-28 16:54:56 +0400

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mandarinchiki.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://pomidoryogurci.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
